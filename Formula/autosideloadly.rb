83f5a40efa97cae3ae876e6ee2bb529a0beec51f
terryso <oxtiger@gmail.com>
2024-02-21 01:48:29 +0800

    更新sha256

--- Formula/autosideloadly.rb
+++ Formula/autosideloadly.rb
@@ -2,7 +2,7 @@ class Autosideloadly < Formula
     desc "AutoSideloadly is a tool that allows you to automatically sideload IPA files to your iOS device using Sideloadly"
     homepage "https://github.com/terryso/AutoSideloadly"
     url "https://github.com/terryso/homebrew-AutoSideloadly/archive/refs/tags/0.0.1.tar.gz"
-    sha256 "fb6e26e31f7cff53efb456c1f15a70b59d6b11f5"
+    sha256 "eb6d4d6cdc1c5274d741316cae24bc49bf70fd3e985682f6ba3e7ee2e1bed5d1"
   
     def install
       bin.install "install_ipa.scpt"
