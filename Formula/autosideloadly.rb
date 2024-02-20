aa7fcd04e76ae4c9df489a8f4a2fc13bcda0cc86
terryso <oxtiger@gmail.com>
2024-02-21 02:32:27 +0800

    bug fix

--- Formula/autosideloadly.rb
+++ Formula/autosideloadly.rb
@@ -2,7 +2,7 @@ class Autosideloadly < Formula
     desc "AutoSideloadly is a tool that allows you to automatically sideload IPA files to your iOS device using Sideloadly"
     homepage "https://github.com/terryso/AutoSideloadly"
     url "https://github.com/terryso/homebrew-AutoSideloadly/archive/refs/tags/0.0.1.tar.gz"
-    sha256 "e4ab2fb026b903da7f1a26e67e16446c16f46c4ec1d1ac106f0c85a693db3ac1"
+    sha256 "23f0dad860b3aa4d8c3a1d9294b297c686d29c26751724462b94cfa4616dd58f"
   
     def install
       bin.install "install_ipa"
