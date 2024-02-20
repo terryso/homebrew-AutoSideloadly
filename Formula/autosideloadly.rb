0fc3fb8ad256e84e6da2f6dab73cab7a87c81bec
terryso <oxtiger@gmail.com>
2024-02-21 01:11:55 +0800

    更新安装文件的sha256

--- Formula/autosideloadly.rb
+++ Formula/autosideloadly.rb
@@ -2,7 +2,7 @@ class AutoSideloadly < Formula
     desc "AutoSideloadly is a tool that allows you to automatically sideload IPA files to your iOS device using Sideloadly"
     homepage "https://github.com/terryso/AutoSideloadly"
     url "https://github.com/terryso/AutoSideloadly/archive/refs/tags/0.0.1.tar.gz"
-    sha256 "put_your_sha256_here"
+    sha256 "c223cbebebabf3ae10c12c2e32b726ec6a8fb597"
   
     def install
       bin.install "install_ipa.scpt"
